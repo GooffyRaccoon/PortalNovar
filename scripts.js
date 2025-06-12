a86b024c21fc9188d22319e5298956aa57cbf79b
Liam <goofyraccoon420@gmail.com>
2025-06-12 10:44:29 -0300

    paralyzed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+<script>
+    let slides = document.querySelectorAll('#slideshow img');
+    let current = 0;
 
+    function showSlide(index) {
+      slides.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+      });
+    }
+
+    setInterval(() => {
+      current = (current + 1) % slides.length;
+      showSlide(current);
+    }, 2000); // Change slide every 2 seconds
+  </script>-->
