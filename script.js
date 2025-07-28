6ccd1bd328480b5a60da59fe75958ba59146c1a9
Liam <goofyraccoon420@gmail.com>
2025-07-28 17:47:44 -0300

    carosel1

---
+++ script.js
@@ -0,0 +1,42 @@
+const slides = document.getElementById('carrosselSlides');
+const indicadores = document.querySelectorAll('.carrossel-indicador');
+const totalSlides = indicadores.length;
+let currentIndex = 0;
+let timer = null;
+
+function mostrarSlide(index) {
+    slides.style.transform = `translateX(-${index * 100}vw)`;
+    indicadores.forEach((ind, i) => {
+        ind.classList.toggle('ativo', i === index);
+    });
+    currentIndex = index;
+}
+
+function proximoSlide() {
+    let nextIndex = (currentIndex + 1) % totalSlides;
+    mostrarSlide(nextIndex);
+}
+
+function iniciarCarrossel() {
+    timer = setInterval(proximoSlide, 8000);
+}
+
+function pararCarrossel() {
+    if (timer) clearInterval(timer);
+}
+
+indicadores.forEach(indicador => {
+    indicador.addEventListener('click', function() {
+        pararCarrossel();
+        mostrarSlide(Number(this.dataset.index));
+        iniciarCarrossel();
+    });
+});
+
+// Ajusta o slide ao redimensionar a tela
+window.addEventListener('resize', () => {
+    mostrarSlide(currentIndex);
+});
+
+mostrarSlide(0);
+iniciarCarrossel();
