f3e71b9918338da54e2a26b6ac5e077b27d56eae
Stephany Valentim <stephany.valentim@escola.pr.gov.br>
2025-09-16 12:01:37 -0300

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://sbfrjfqaraxfulwqselv.supabase.co"; // substitua
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiZnJqZnFhcmF4ZnVsd3FzZWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg2ODUsImV4cCI6MjA3MzUxNDY4NX0.YPJ0zYeXU6UtacOCWJ2JM6NAUQIu0WwrN3B8aQiLMY0"; // substitua (anon public key)
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
