272269a907725a3ecb915506667260d8db5c8685
Stephany Valentim <stephany.valentim@escola.pr.gov.br>
2025-09-22 08:13:00 -0300

    Update cadlog.js

--- cadlog.js
+++ cadlog.js
@@ -1,10 +1,8 @@
 // app.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm&quot;;
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// ===== CONFIG SUPABASE =====
-const SUPABASE_URL = "https://sbfrjfqaraxfulwqselv.supabase.co;;   // troque
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiZnJqZnFhcmF4ZnVsd3FzZWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg2ODUsImV4cCI6MjA3MzUxNDY4NX0.YPJ0zYeXU6UtacOCWJ2JM6NAUQIu0WwrN3B8aQiLMY0";             // troque
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);cOCWJ2JM6NAUQIu0WwrN3B8aQiLMY0"; // substitua (anon public key)
+const SUPABASE_URL = "https://sbfrjfqaraxfulwqselv.supabase.co"; // substitua
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiZnJqZnFhcmF4ZnVsd3FzZWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg2ODUsImV4cCI6MjA3MzUxNDY4NX0.YPJ0zYeXU6UtacOCWJ2JM6NAUQIu0WwrN3B8aQiLMY0"; // substitua (anon public key)
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 // --- DOM Elements ---
