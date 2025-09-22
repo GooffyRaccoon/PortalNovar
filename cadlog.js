2832a8e0f158276a8f76af79c9278288f7cc6162
Stephany Valentim <stephany.valentim@escola.pr.gov.br>
2025-09-22 08:36:00 -0300

    Update cadlog.js

--- cadlog.js
+++ cadlog.js
@@ -6,8 +6,6 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 // --- DOM Elements ---
 const tabLogin = document.getElementById("tabLogin");
 const tabRegister = document.getElementById("tabRegister");
